bfa98b40a8bd98a86ddc99e00e6cec6043b8cae5
binhlethanh201 <ltbinh04@gmail.com>
2026-01-21 17:05:26 +0700

    update cors

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const port = process.env.PORT || 9999;
 const connectDB = require("./config/db");
 const router = require("./routes/index");
 const cors = require("cors");
+const morgan = require('morgan');
 
 app.use(cors());
-// app.use(
-//   cors({
-//     origin: ["http://localhost:8081"],
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:8081"],
+  })
+);
 
+app.use(morgan('dev'))
 app.use(express.json());
 connectDB();
 
