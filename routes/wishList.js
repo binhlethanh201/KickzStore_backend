f87160e887d6f594bad8617e8709de420f79ada0
binhlethanh201 <ltbinh04@gmail.com>
2026-03-13 10:25:50 +0700

    fixing case insentive

--- routes/wishList.js
+++ routes/wishList.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const wishlistController = require("../controllers/wishlistController");
+const wishlistController = require("../controllers/wishListController");
 const verifyToken = require("../middlewares/auth");
 
 router.get("/", wishlistController.getAll);
